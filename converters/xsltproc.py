f7784e878a0a55b8fffa7d64b222e3cabd0d14cb
Ron <45816308+rjaegers@users.noreply.github.com>
2025-06-17 15:38:23 +0200

    chore: paths...

--- converters/xsltproc.py
+++ converters/xsltproc.py
@@ -19,7 +19,19 @@ if __name__ == "__main__":
     parser.add_argument("--workdir", default=".", help="Working directory to resolve relative paths.")
     args = parser.parse_args()
 
+    root_path = os.path.abspath('.').split(os.path.sep)[0] + os.path.sep
     transformer = etree.XSLT(open_etree(Path(args.xslt_file)))
+    resolved_paths = []
+
     for xml_path in args.xml_paths:
-        for path in Path(args.workdir).glob(xml_path):
-            sys.stdout.write(str(transformer(open_etree(path))))
+        p = Path(xml_path)
+
+        if p.is_file():
+            resolved_paths.append(p)
+        elif p.is_absolute():
+            resolved_paths.extend(Path(root_path).glob(xml_path[len(root_path):]))
+        else:
+            resolved_paths.extend(Path(args.workdir).glob(xml_path))
+
+    for path in resolved_paths:
+        sys.stdout.write(str(transformer(open_etree(path))))
